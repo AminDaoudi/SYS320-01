8624e0f59dd6424c6c6358e9518a533e0d70424a
AminDaoudi <89942514+AminDaoudi@users.noreply.github.com>
2024-02-29 23:07:07 -0500

    USERS1.ps1

--- week6/USERS1.ps1
+++ week6/USERS1.ps1
@@ -40,10 +40,10 @@ function createAUser($name, $password){
 
    # ***** Policies ******
 
-   # User should be forced to change password
+  # User should be forced to change password
    Set-LocalUser $newUser -PasswordNeverExpires $false
 
-   # First time created users should be disabled
+ # First time created users should be disabled
    Disable-LocalUser $newUser
 
 }
@@ -60,22 +60,19 @@ function removeAUser($name){
 
 
 function disableAUser($name){
-   
-   $userToBeDeleted = Get-LocalUser | Where-Object { $_.name -ilike $name }
+  $userToBeDeleted = Get-LocalUser | Where-Object { $_.name -ilike $name }
    Disable-LocalUser $userToBeDeleted
    
 }
 
 
 function enableAUser($name){
-   
-   $userToBeEnabled = Get-LocalUser | Where-Object { $_.name -ilike $name }
+  $userToBeEnabled = Get-LocalUser | Where-Object { $_.name -ilike $name }
    Enable-LocalUser $userToBeEnabled
    
 }
 function checkUser($name) {
-
-    $Users = Get-LocalUser | Where-Object { $_.name -ilike $name }
+  $Users = Get-LocalUser | Where-Object { $_.name -ilike $name }
 
     if ($Users.Count -gt 0) {
         return $true
@@ -86,17 +83,13 @@ function checkUser($name) {
 } 
 
 function riskUsers($days) {
-    # Placeholder for retrieving failed login attempts within the specified number of days
-    # This needs to be replaced with your actual logic for retrieving this data
-    $failedLogins = getFailedLogins $days
+   $failedLogins = getFailedLogins $days
 
-    # Group failed login attempts by username
+    #failed login attempts by username
     $groupedLogins = $failedLogins | Group-Object User
 
-    # Filter out users with more than 10 failed login attempts
     $atRiskUsers = $groupedLogins | Where-Object { $_.Count -gt 1 }
-
-    # Display or return the list of at-risk users
+    #return the list of at-risk users
     $atRiskUsers | ForEach-Object {
         Write-Host "User at risk: $($_.Name) with $($_.Count) failed login attempts in the last $days days."
     }
