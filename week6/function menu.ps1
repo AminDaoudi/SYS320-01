5aa17a08ebce6ed397ecec787454873ab07ea10d
AminDaoudi <89942514+AminDaoudi@users.noreply.github.com>
2024-02-29 23:38:50 -0500

    Add files via upload

---
+++ week6/function menu.ps1
@@ -0,0 +1,30 @@
+﻿
+#Function 
+function Get-LastApacheLogs {
+    # Define the path to the Apache log file
+    $logPath = "C:\xampp\apache\logs\access.log"
+
+    # Check if the log file exists
+    if (Test-Path $logPath) {
+        # Display the last 10 entries from the log file
+        Get-Content $logPath -Tail 10
+    } else {
+        Write-Error "Log file does not exist at the path: $logPath"
+    }
+}
+
+function Manage-ChromeProcess {
+    $chromeProcess = Get-Process "chrome" -ErrorAction SilentlyContinue
+
+    if ($chromeProcess -eq $null) {
+     Start-Process "chrome" -ArgumentList "https://www.champlain.edu"
+    } else {
+  
+  
+  
+  
+  
+  
+        Stop-Process -Name "chrome" -Force
+    }
+}
