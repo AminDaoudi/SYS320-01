a90dac5dbdf8184f2d85c0a4cb68426eec1712a6
AminDaoudi <89942514+AminDaoudi@users.noreply.github.com>
2024-05-02 14:17:35 -0400

    c2.bash

--- c2.bash
+++ c2.bash
@@ -1,12 +1,9 @@
 #!/bin/bash
-
 #checking the number of correct arguments
-
 if [ "$#" ----ne 2 ]; then
     echo "Usage: $0 <log_file> <ioc_file>"
-    exit 1
-fi
-
+    exit 1 
+    fi
 #arguments to variables
 log_file="$1"
 ioc_file="$2"
@@ -14,5 +11,4 @@ ioc_file="$2"
 while IFS= read -r ioc_pattern; do
     grep "$ioc_pattern" "$log_file" | awk '{sub(/^\[/, "", $4); print $1, $4, $7}' >> report.txt
 done < "$ioc.txt"
-
 echo "saved to report.txt"
